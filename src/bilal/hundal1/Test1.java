c13d416d7284863e5043da8930fb775b50ba4c0e
hs <hs@UMC-03456662441.Home>
2021-06-18 18:25:06 +0100

    FirstCommit

---
+++ src/bilal/hundal1/Test1.java
@@ -0,0 +1,13 @@
+package bilal.hundal1;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungary= "yes";
+		if(hungary.equalsIgnoreCase("yest")) {
+			System.out.println("Eat Something");
+		}else {
+			System.out.println("Do Work...");
+		}
+	}
+
+}
